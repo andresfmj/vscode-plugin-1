b2514beba621b91cb20c6bc4fb2df9ecdc732292
Andres Fernando <andres.fernando08@gmail.com>
2021-11-18 23:29:14 -0500

    fetch data from json file with axios

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,22 @@
 import { commands, ExtensionContext, window } from "vscode";
+import axios from 'axios';
 
 export function activate(context: ExtensionContext) {
-  let helloPlugin = async () => {
-    window.showInformationMessage("Hello This is Plugin of test");
-  }
+	let helloPlugin = async () => {
+		window.showInformationMessage('Hello This is Plugin of test');
 
-  let command = commands.registerCommand(
-    "helloPlugin",
-    helloPlugin
-  );
-  context.subscriptions.push(command);
+		let editor = window.activeTextEditor;
+		if (editor) {
+			const user = await axios.get('http://localhost:3000/user');
+			const data = user.data;
+
+			editor?.edit((editBuilder) => {
+				let position = editor?.selection.active;
+				position && editBuilder.insert(position, `Hello ${data.name}`);
+			});
+		}
+	};
+
+	let command = commands.registerCommand('helloPlugin', helloPlugin);
+	context.subscriptions.push(command);
 }
