8c07669b3ecd31aff618ec58940008e9e9614e68
Andres Fernando <andres.fernando08@gmail.com>
2021-11-19 20:51:24 -0500

    [add] doing some test by sending data through put or post method

--- src/extension.ts
+++ src/extension.ts
@@ -7,12 +7,30 @@ export function activate(context: ExtensionContext) {
 
 		let editor = window.activeTextEditor;
 		if (editor) {
-			const user = await axios.get('http://localhost:3000/user');
-			const data = user.data;
+			const user = {
+				name: 'andresfmj',
+				settings: {
+					darkMode: true,
+					language: 'en',
+				},
+			};
+			const newUserSetting = await axios.put(
+				'http://localhost:3000/user',
+				user,
+				{
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			);
 
 			editor?.edit((editBuilder) => {
 				let position = editor?.selection.active;
-				position && editBuilder.insert(position, `Hello ${data.name}`);
+				const message =
+					`Hello ${user.name}\n` +
+					`Your settings are: darkMode: ${user.settings.darkMode}\n` +
+					` and language: ${user.settings.language}\n`;
+				position && editBuilder.insert(position, message);
 			});
 		}
 	};
